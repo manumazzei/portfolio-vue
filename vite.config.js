846f5b199aa8ce6f011413d41417f37cac5dc434
Emmanuelle Matos <emmanuelle.matos@4network.com>
2023-08-11 15:52:05 -0300

    arruma build.

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: process.env.NODE_ENV === 'production'
-    ? '/portfolio-vue/'
-    : '/'
+ // base: process.env.NODE_ENV === 'production'
+ //  ? '/portfolio-vue/'
+ //   : '/'
 })
